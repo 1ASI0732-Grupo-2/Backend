1eec433a955c120cbedcec7a97d1411e75afbec0
Rodrigo Liberato <U202215623@upc.edu.pe>
2025-11-04 14:25:58 -0500

    fix(CreateUserCommand): deleted unnecessary info to create user

--- UserContext/Domain/Models/Commands/CreateUserCommand.cs
+++ UserContext/Domain/Models/Commands/CreateUserCommand.cs
@@ -10,10 +10,6 @@ namespace workstation_backend.UserContext.Domain.Models.Commands;
 /// <param name="PhoneNumber">Número de teléfono de contacto.</param>
 /// <param name="Email">Correo electrónico del usuario.</param>
 /// <param name="Role">Rol asignado al usuario (ej. Buscador, Alquilador).</param>
-/// <param name="Nickname">Apodo o alias del usuario (opcional).</param>
-/// <param name="Description">Descripción personal del usuario (opcional).</param>
-/// <param name="BusinessName">Nombre del negocio del usuario (opcional).</param>
-/// <param name="ExtraInfo">Información adicional relevante (opcional).</param>
 public record CreateUserCommand(
     string FirstName,
     string LastName,
@@ -21,9 +17,5 @@ public record CreateUserCommand(
     string PhoneNumber,
     string Email,
     String PasswordHash,
-    UserRole Role,
-    string? Nickname,       
-    string? Description,    
-    string? BusinessName,   
-    string? ExtraInfo       
+    UserRole Role      
 );
